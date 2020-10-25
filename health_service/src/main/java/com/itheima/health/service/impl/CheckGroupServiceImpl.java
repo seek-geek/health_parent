188fcf150e950fc8c79e76ccc12b878c3374054f
lan <lanbixing@qq.com>
2020-10-25 19:24:08 +0800

    添加选项组功能

--- health_service/src/main/java/com/itheima/health/service/impl/CheckGroupServiceImpl.java
+++ health_service/src/main/java/com/itheima/health/service/impl/CheckGroupServiceImpl.java
@@ -1,6 +1,8 @@
 package com.itheima.health.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.itheima.health.dao.CheckGroupDao;
 import com.itheima.health.entity.PageResult;
 import com.itheima.health.entity.QueryPageBean;
@@ -8,6 +10,7 @@ import com.itheima.health.exception.MyException;
 import com.itheima.health.pojo.CheckGroup;
 import com.itheima.health.service.CheckGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -20,31 +23,54 @@ public class CheckGroupServiceImpl implements CheckGroupService {
 
     @Override
     public void add(CheckGroup checkGroup, Integer[] checkitemIds) {
-
+        checkGroupDao.add(checkGroup);
+        Integer checkGroupId = checkGroup.getId();
+        if (checkitemIds != null) {
+            for (Integer checkitemId : checkitemIds) {
+                checkGroupDao.addCheckGroupCheckItem(checkGroupId, checkitemId);
+            }
+        }
     }
 
     @Override
     public PageResult<CheckGroup> findPage(QueryPageBean queryPageBean) {
-        return null;
+        PageHelper.startPage(queryPageBean.getCurrentPage(),queryPageBean.getPageSize());
+        if(!StringUtils.isEmpty(queryPageBean.getQueryString())){
+            queryPageBean.setQueryString("%"+ queryPageBean.getQueryString()+"%");
+        }
+        Page<CheckGroup> page = checkGroupDao.findByCondition(queryPageBean.getQueryString());
+        PageResult<CheckGroup> pageResult = new PageResult<>(page.getTotal(),page.getResult());
+        return pageResult;
     }
 
     @Override
     public CheckGroup findById(int id) {
-        return null;
+        return checkGroupDao.findById(id);
     }
 
     @Override
     public List<Integer> findCheckItemIdsByCheckGroupId(int id) {
-        return null;
+        return checkGroupDao.findCheckItemIdsByCheckGroupId(id);
     }
 
     @Override
     public void update(CheckGroup checkGroup, Integer[] checkitemIds) {
-
+        checkGroupDao.update(checkGroup);
+        checkGroupDao.deleteCheckGroupCheckItem(checkGroup.getId());
+        if(null != checkitemIds){
+            for (Integer checkitemId : checkitemIds) {
+                checkGroupDao.addCheckGroupCheckItem(checkGroup.getId(), checkitemId);
+            }
+        }
     }
 
     @Override
     public void deleteById(int id) throws MyException {
-
+        int count = checkGroupDao.findCountByCheckGroupId(id);
+        if (count > 0){
+            throw new MyException("该检查组已经被套餐使用了，不能删除");
+        }
+        checkGroupDao.deleteCheckGroupCheckItem(id);
+        checkGroupDao.deleteById(id);
     }
 }
--- health_web/src/main/java/com/itheima/health/controller/CheckGroupController.java
+++ health_web/src/main/java/com/itheima/health/controller/CheckGroupController.java
@@ -2,13 +2,14 @@ package com.itheima.health.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.itheima.health.constant.MessageConstant;
+import com.itheima.health.entity.PageResult;
+import com.itheima.health.entity.QueryPageBean;
 import com.itheima.health.entity.Result;
 import com.itheima.health.pojo.CheckGroup;
 import com.itheima.health.service.CheckGroupService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/checkgroup")
@@ -20,5 +21,35 @@ public class CheckGroupController {
     public Result add(@RequestBody CheckGroup checkGroup, Integer[] checkitemIds) {
         checkGroupService.add(checkGroup, checkitemIds);
         return new Result(true, MessageConstant.ADD_CHECKGROUP_SUCCESS);
+    }
+
+    @PostMapping("/findPage")
+    public Result findPage(@RequestBody QueryPageBean queryPageBean){
+        PageResult<CheckGroup> pageResult = checkGroupService.findPage(queryPageBean);
+        return new Result(true, MessageConstant.QUERY_CHECKGROUP_SUCCESS,pageResult);
+    }
+
+    @GetMapping("/findById")
+    public Result findById(int id){
+        CheckGroup checkGroup = checkGroupService.findById(id);
+        return new Result(true, MessageConstant.QUERY_CHECKGROUP_SUCCESS,checkGroup);
+    }
+
+    @GetMapping("/findCheckItemIdsByCheckGroupId")
+    public Result findCheckItemIdsByCheckGroupId(int id){
+        List<Integer> checkitemIds = checkGroupService.findCheckItemIdsByCheckGroupId(id);
+        return new Result(true, MessageConstant.QUERY_CHECKGROUP_SUCCESS,checkitemIds);
+    }
+
+    @PostMapping("/update")
+    public Result update(@RequestBody CheckGroup checkGroup, Integer[] checkitemIds){
+        checkGroupService.update(checkGroup,checkitemIds);
+        return new Result(true, MessageConstant.EDIT_CHECKGROUP_SUCCESS);
+    }
+
+    @PostMapping("/deleteById")
+    public Result deleteById(int id){
+        checkGroupService.deleteById(id);
+        return new Result(true, MessageConstant.DELETE_CHECKGROUP_SUCCESS);
     }
 }
